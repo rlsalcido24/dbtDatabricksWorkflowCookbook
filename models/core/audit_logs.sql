af16ccf9350670fc8aba93a7269b948ed2369a75
dustinvannoy-db <dustin.vannoy@databricks.com>
2024-03-14 22:16:15 -0700

    Add initial audit_logs core table

--- models/core/audit_logs.sql
+++ models/core/audit_logs.sql
@@ -1,1 +1,5 @@
--- todo
+select *
+from {{ ref('raw_audit_logs') }}
+where action_name in ('executeAdhocQuery', 'submitRun', 'create', 'createTable', 'delete', 'update', 'edit', 'attachNotebook', 'detachNotebook', 'fileCreate', 'startUpdate'
+  ,'createResult', 'createQuery', 'runStart', 'executeSavedQuery', 'runSucceeded', 'updateQuery', 'runTriggered', 'runNow', 'createNotebook', 'restartResult', 'startResult'
+)
